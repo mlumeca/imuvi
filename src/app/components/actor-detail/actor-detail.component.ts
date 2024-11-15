96f54e6ca82c6f3ee76da97b44dcb3084a058833
candi <alcantarilla.cacan24@gmail.com>
2024-11-15 14:26:56 +0100

    no me pilla la variable del ts

--- src/app/components/actor-detail/actor-detail.component.ts
+++ src/app/components/actor-detail/actor-detail.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActorDetailResponse } from '../../models/actor-detail.interface';
+import { ActorService } from '../../services/actor.service';
 
 @Component({
   selector: 'app-actor-detail',
   templateUrl: './actor-detail.component.html',
   styleUrl: './actor-detail.component.css'
 })
-export class ActorDetailComponent {
+export class ActorDetailComponent implements OnInit{
+
+  actorId: number | undefined;
+  actorDetail: ActorDetailResponse | undefined;
+
+  constructor( private actorService : ActorService){}
+  
+  ngOnInit(): void {
+    this.actorService.getOneActor(this.actorId!).subscribe((response) => {
+      this.actorDetail = response;
+    })
+  }
 
 }
--- src/app/services/actor.service.ts
+++ src/app/services/actor.service.ts
@@ -1,11 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {ActorDetailResponse} from '../models/actor-detail.interface';
 
-const API_KEY = 'd3faeb037eb779bc62a224025b2f279e';
+const API_KEY = '6ea0f745f14c15097ec90310753616a0';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActorService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getOneActor(id: number): Observable<ActorDetailResponse>{
+    return this.http.get<ActorDetailResponse>(`https://api.themoviedb.org/3/person/${id}?api_key=${API_KEY}`)
+  }
 }
