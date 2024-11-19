3b0ba27f0b1df4e9c8c9e9304db47a37b018175a
Candi Alcantarilla <alcantarilla.cacan24@triana.salesianos.edu>
2024-11-19 21:53:47 +0100

    ultimos detalles terminados

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { MovieListResponse } from '../models/movie-list.interface';
 import { MovieCreditResponse, MovieDetailResponse, MovieMediaResponse, MoviePlatformResponse } from '../models/movie-detail.interface';
 import { MovieGenreResponse, MovieListResponse } from '../models/movie-list.interface';
 
@@ -14,31 +13,31 @@ export class MovieService {
 
   constructor(private http: HttpClient) { }
 
-  getMovieList(): Observable<MovieListResponse>{
+  getMovieList(): Observable<MovieListResponse> {
     return this.http.get<MovieListResponse>(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
   }
 
-  getOneMovie(id: string): Observable<MovieDetailResponse>{
+  getOneMovie(id: string): Observable<MovieDetailResponse> {
     return this.http.get<MovieDetailResponse>(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
   }
 
-  getMovieCredits(id: string): Observable<MovieCreditResponse>{
+  getMovieCredits(id: string): Observable<MovieCreditResponse> {
     return this.http.get<MovieCreditResponse>(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_KEY}`)
   }
 
-  getTranslatedTitle(id: string): Observable<MovieCreditResponse>{
+  getTranslatedTitle(id: string): Observable<MovieCreditResponse> {
     return this.http.get<MovieCreditResponse>(`https://api.themoviedb.org/3/movie/${id}/alternative_titles?api_key=${API_KEY}`)
   }
 
-  getPlatforms(id: string): Observable<MoviePlatformResponse>{
+  getPlatforms(id: string): Observable<MoviePlatformResponse> {
     return this.http.get<MoviePlatformResponse>(`https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=${API_KEY}`)
   }
 
-  getMedia(id: string): Observable<MovieMediaResponse>{
+  getMedia(id: string): Observable<MovieMediaResponse> {
     return this.http.get<MovieMediaResponse>(`https://api.themoviedb.org/3/movie/${id}/images?api_key=${API_KEY}`)
   }
 
-  getMovieGenre(): Observable<MovieGenreResponse>{
+  getMovieGenre(): Observable<MovieGenreResponse> {
     return this.http.get<MovieGenreResponse>(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
   }
 }
