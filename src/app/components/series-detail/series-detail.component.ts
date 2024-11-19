787df85ca14bb964e5d1593493c2f42367b33a87
Candi Alcantarilla <alcantarilla.cacan24@triana.salesianos.edu>
2024-11-19 21:25:27 +0100

    detalle seriees terminado

--- src/app/components/series-detail/series-detail.component.ts
+++ src/app/components/series-detail/series-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Cast, Crew, Episode, Flatrate25, SeasonsResponse, SeriesCreditResponse, SeriesDetailResponse, SeriesMediaResponse } from '../../models/series-detail.interface';
+import { Cast, Crew, Episode, Flatrate25, Season, SeriesCreditResponse, SeriesDetailResponse, SeriesMediaResponse } from '../../models/series-detail.interface';
 import { SerieService } from '../../services/serie.service';
 
 @Component({
@@ -8,48 +8,44 @@ import { SerieService } from '../../services/serie.service';
   templateUrl: './series-detail.component.html',
   styleUrl: './series-detail.component.css'
 })
-export class SeriesDetailComponent implements OnInit{
+export class SeriesDetailComponent implements OnInit {
   seriesId: string | null = '';
-  seasonNumber: string | null = '';
   oneSeries: SeriesDetailResponse | undefined;
   seriesCredits: SeriesCreditResponse | undefined;
   cast: Cast[] = [];
   crew: Crew[] = [];
   buyPlatform: Flatrate25[] = [];
-  imgMedia: SeriesMediaResponse | undefined; 
-  seasons: SeasonsResponse | undefined; 
-  episodes: Episode[] = [];
+  imgMedia: SeriesMediaResponse | undefined;
+  seasons: Season[] = [];
 
-  constructor(private seriesService: SerieService, private route: ActivatedRoute) {}
+  constructor(private seriesService: SerieService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.seriesId = this.route.snapshot.paramMap.get('id');
-    this.seasonNumber = this.route.snapshot.paramMap.get('season_number');
 
-    this.seriesService.getOneSeries(this.seriesId!).subscribe (response => {
+    this.seriesService.getOneSeries(this.seriesId!).subscribe(response => {
       this.oneSeries = response;
     })
 
-    this.seriesService.getCredits(this.seriesId!).subscribe (response => {
+    this.seriesService.getCredits(this.seriesId!).subscribe(response => {
       this.cast = response.cast;
     })
 
-    this.seriesService.getCredits(this.seriesId!).subscribe (response => {
+    this.seriesService.getCredits(this.seriesId!).subscribe(response => {
       this.crew = response.crew;
     })
 
-    this.seriesService.getPlatforms(this.seriesId!).subscribe (response => {
+    this.seriesService.getPlatforms(this.seriesId!).subscribe(response => {
       this.buyPlatform = response.results.ES.flatrate;
     })
 
-    this.seriesService.getMedia(this.seriesId!).subscribe (response => {
+    this.seriesService.getMedia(this.seriesId!).subscribe(response => {
       this.imgMedia = response;
     })
 
-    this.seriesService.getSeasons(this.seriesId!, this.seasonNumber!).subscribe (response => {
-      this.episodes = response.episodes;
+    this.seriesService.getOneSeries(this.seriesId!).subscribe(response => {
+      this.seasons = response.seasons;
     })
-
   }
 
   getImagen(url: string): string {
@@ -65,6 +61,7 @@ export class SeriesDetailComponent implements OnInit{
   }
 
   getImgChapter(url: string): string {
-    return 'https://media.themoviedb.org/t/p/w227_and_h127_bestv2' + url;
+    return 'https://media.themoviedb.org/t/p/w130_and_h195_bestv2' + url;
   }
+
 }
--- src/app/services/serie.service.ts
+++ src/app/services/serie.service.ts
@@ -13,35 +13,32 @@ export class SerieService {
 
   constructor(private http: HttpClient) { }
 
-  getSerieList(): Observable<SerieListResponse>{
+  getSerieList(): Observable<SerieListResponse> {
     return this.http.get<SerieListResponse>(`https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}`)
   }
 
-  getSerieGenre(): Observable<SerieGenreResponse>{
+  getSerieGenre(): Observable<SerieGenreResponse> {
     return this.http.get<SerieGenreResponse>(`https://api.themoviedb.org/3/genre/tv/list?api_key=${API_KEY}`);
   }
 
-  getOneSeries(id: string): Observable<SeriesDetailResponse>{
+  getOneSeries(id: string): Observable<SeriesDetailResponse> {
     return this.http.get<SeriesDetailResponse>(`https://api.themoviedb.org/3/tv/${id}?api_key=${API_KEY}`)
   }
-  
-  getCredits(id: string): Observable<SeriesCreditResponse>{
+
+  getCredits(id: string): Observable<SeriesCreditResponse> {
     return this.http.get<SeriesCreditResponse>(`https://api.themoviedb.org/3/tv/${id}/credits?api_key=${API_KEY}`)
   }
 
-  getTranslatedTitle(id: string): Observable<SeriesCreditResponse>{
+  getTranslatedTitle(id: string): Observable<SeriesCreditResponse> {
     return this.http.get<SeriesCreditResponse>(`https://api.themoviedb.org/3/tv/${id}/alternative_titles?api_key=${API_KEY}`)
   }
 
-  getPlatforms(id: string): Observable<SeriesPlatformResponse>{
+  getPlatforms(id: string): Observable<SeriesPlatformResponse> {
     return this.http.get<SeriesPlatformResponse>(`https://api.themoviedb.org/3/tv/${id}/watch/providers?api_key=${API_KEY}`)
   }
 
-  getMedia(id: string): Observable<SeriesMediaResponse>{
+  getMedia(id: string): Observable<SeriesMediaResponse> {
     return this.http.get<SeriesMediaResponse>(`https://api.themoviedb.org/3/tv/${id}/images?api_key=${API_KEY}`)
   }
 
-  getSeasons(id: string, season_number: string): Observable<SeasonsResponse>{
-    return this.http.get<SeasonsResponse>(`https://api.themoviedb.org/3/tv/${id}/season/${season_number}?api_key=${API_KEY}`)
-  }
 }
