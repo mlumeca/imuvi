3d0150fded9ac7f90eada40e7742b765191caf5a
Olga <olga.valor.wu@gmail.com>
2024-11-19 18:35:37 +0100

    home acabado

--- src/app/models/home.interface.ts
+++ src/app/models/home.interface.ts
@@ -4,7 +4,7 @@ export interface TrendingResponse {
     total_pages: number
     total_results: number
 }
-  
+
 export interface Trending {
     backdrop_path: string
     id: number
@@ -26,4 +26,72 @@ export interface Trending {
     release_date?: string
     video?: boolean
 }
-  
+
+export interface MovieVideoResponse {
+    id: number
+    results: MovieVideo[]
+}
+
+export interface MovieVideo {
+    iso_639_1: string
+    iso_3166_1: string
+    name: string
+    key: string
+    site: string
+    size: number
+    type: string
+    official: boolean
+    published_at: string
+    id: string
+}
+
+
+export interface TrendingActorsResponse {
+    page: number
+    results: Actor[]
+    total_pages: number
+    total_results: number
+}
+
+export interface Actor {
+    id: number
+    name: string
+    original_name: string
+    media_type: string
+    adult: boolean
+    popularity: number
+    gender: number
+    known_for_department?: string
+    profile_path?: string
+}
+
+
+export interface UpcomingMoviesResponse {
+    dates: Dates
+    page: number
+    results: UpcomingMovies[]
+    total_pages: number
+    total_results: number
+}
+
+export interface Dates {
+    maximum: string
+    minimum: string
+}
+
+export interface UpcomingMovies {
+    adult: boolean
+    backdrop_path: string
+    genre_ids: number[]
+    id: number
+    original_language: string
+    original_title: string
+    overview: string
+    popularity: number
+    poster_path: string
+    release_date: string
+    title: string
+    video: boolean
+    vote_average: number
+    vote_count: number
+}
--- src/app/services/imuvi.service.ts
+++ src/app/services/imuvi.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { TrendingResponse } from '../models/home.interface';
+import { MovieVideoResponse, TrendingResponse, UpcomingMoviesResponse } from '../models/home.interface';
 import { TrendingActorsResponse } from '../models/homeActors.interface';
 
 const API_KEY = 'd3faeb037eb779bc62a224025b2f279e';
@@ -20,5 +20,13 @@ export class ImuviService {
 
   getTrendingActor(): Observable<TrendingActorsResponse> {
     return this.http.get<TrendingActorsResponse>(`https://api.themoviedb.org/3/trending/person/week?api_key=${API_KEY}`)
+  }
+
+  getUpcomingMovies(): Observable<UpcomingMoviesResponse> {
+    return this.http.get<UpcomingMoviesResponse>(`https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`);
+  }
+  
+  getMovieVideoById(movieId: number): Observable<MovieVideoResponse> {
+    return this.http.get<MovieVideoResponse>(`https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}`);
   }
 }
