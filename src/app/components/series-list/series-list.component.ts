eb6f021481a7ef9b008e8096f1057b9587a52b70
candi <alcantarilla.cacan24@gmail.com>
2024-11-26 12:40:49 +0100

    terminado filtrado y busquedas

--- src/app/components/series-list/series-list.component.ts
+++ src/app/components/series-list/series-list.component.ts
@@ -11,10 +11,20 @@ export class SeriesListComponent {
   serieList: Serie[] = [];
   originalSerieList: Serie[] = [];
   serieGenre: SerieGenre[] = [];
+
   page = 1;
   totalPages = 1;
 
+  selectedGenres: number[] = [];
+
+  minRating: number = 0;
+  maxRating: number = 10;
+
   @Input() texto = '';
+
+  firstAirDateFrom: string = '';
+  firstAirDateTo: string = '';
+
   constructor(private serieService: SerieService) { }
 
 
@@ -37,7 +47,7 @@ export class SeriesListComponent {
   newPage(): void {
     this.serieService.getSeriePage(this.page).subscribe(resp => {
       this.serieList = resp.results;
-      this.originalSerieList = [...this.serieList]; // Almacenar la lista original aquí
+      this.originalSerieList = this.serieList;
       this.totalPages = resp.total_pages;
     });
   }
@@ -45,6 +55,52 @@ export class SeriesListComponent {
   onPage(newPage: number): void {
     this.page = newPage;
     this.newPage();
+  }
+
+  filterByGenre(): void {
+    if (this.selectedGenres.length === 0) {
+      this.originalSerieList = this.serieList;
+    } else {
+      this.originalSerieList = this.serieList.filter(serie =>
+        serie.genre_ids.some(genreId => this.selectedGenres.includes(genreId))
+      );
+    }
+  }
+
+  onGenreChange(genreId: number, isChecked: boolean): void {
+    if (isChecked) {
+      if (!this.selectedGenres.includes(genreId)) {
+        this.selectedGenres.push(genreId);
+      }
+    } else {
+      this.selectedGenres = this.selectedGenres.filter(id => id !== genreId);
+    }
+    this.applyFilters();
+  }
+
+  applyFilters(): void {
+    this.originalSerieList = this.serieList;
+
+    if (this.selectedGenres.length > 0) {
+      this.originalSerieList = this.originalSerieList.filter(serie =>
+        serie.genre_ids.some(genreId => this.selectedGenres.includes(genreId))
+      );
+    }
+
+    this.originalSerieList = this.originalSerieList.filter(serie =>
+      serie.vote_average >= this.minRating && serie.vote_average <= this.maxRating
+    );
+
+    if (this.firstAirDateFrom) {
+      this.originalSerieList = this.originalSerieList.filter(serie =>
+        new Date(serie.first_air_date) >= new Date(this.firstAirDateFrom)
+      );
+    }
+    if (this.firstAirDateTo) {
+      this.originalSerieList = this.originalSerieList.filter(serie =>
+        new Date(serie.first_air_date) <= new Date(this.firstAirDateTo)
+      );
+    }
   }
 
   searchingSerie(name: string): void {
